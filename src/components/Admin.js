43c5447765147b41e5e2529a7ea693df8c04f479
NobelRoyP <101279727+NobelRoyP@users.noreply.github.com>
2022-05-01 14:29:56 +0530

    Update Admin.js

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -6,8 +6,8 @@ import { Link } from 'react-router-dom'
 const Admin = () => {
     return (
         <div className="admin-area">
-            <form method="POST" name="admin-add-file" data-netlify="true">
-                <input type="file" className="btn btn-warning" /><br /><br />
+            <form action="POST" name="admin-add-file" data-netlify="true">
+                <input type="file" className="btn btn-warning" name="Photo" id="photo"/><br /><br />
                 <button type="submit" className="btn btn-primary">Add</button>
                 <Link to='/' className="btn btn-danger">Cancel</Link>
             </form>
