bd6c53fc2925b53c88c275e335e8366bb4e4ecc5
NobelRoyP <101279727+NobelRoyP@users.noreply.github.com>
2022-05-01 21:18:55 +0530

    Update Admin.js

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 const Admin = () => {
     return (
         <div className="admin-area">
-            <form name="form1" method="post" netlify>
+            <form name="form1" method="post" data-netlify="true">
                 <input type="text" name="name"/>
                 <input type="text"/>
                 <button type="submit">Send</button>
