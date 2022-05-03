b95f2330cc49e2b4f678e85219e67f0c8e7fbf87
NobelRoyP <101279727+NobelRoyP@users.noreply.github.com>
2022-05-03 09:51:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import About from "./components/About";
 import Home from "./components/Home";
 import Contact from './components/Contact';
-import Admin from './components/Admin';
+import Admin from './components/add-form.html';
 import './App.css';
 
 function App() {
