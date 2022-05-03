8a444817f59539d5aabce8bf186761906d822f48
NobelRoyP <101279727+NobelRoyP@users.noreply.github.com>
2022-05-03 10:01:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           <Route path="/" element={<Home/>} />
           <Route path="/about" element={<About/>} />
           <Route path="/contact" element={<Contact/>} />
-          <Link to="./components/add-form.html" className="btn btn-success">Add</Link>
+          <a href="./components/add-form.html" className="btn btn-success">Add</a>
         </Routes>
         <Header />
       </Router>
