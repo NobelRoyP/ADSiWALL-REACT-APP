6157cfc33e073d2bce61a917f1a37f4064c0a9a1
NobelRoyP <101279727+NobelRoyP@users.noreply.github.com>
2022-05-04 21:13:55 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,6 @@ import React from 'react'
 import logo from './favicon.png';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Link } from 'react-router-dom';
-import  ../App.css;
 
 const Header = () => {
 
