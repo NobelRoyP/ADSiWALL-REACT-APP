5893089d18cfe9ab76ef171e96d3c112b08caff1
NobelRoyP <101279727+NobelRoyP@users.noreply.github.com>
2022-05-06 00:24:52 +0530

    Add files via upload

--- src/components/AddPhoto.js
+++ src/components/AddPhoto.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import '../AddPhoto.css'
 import 'bootstrap'
+import { Link } from 'react-router-dom'
 
-function AddPhoto() {
+const AddPhoto = () => {
     function getImagePreview(event) {
         var image = URL.createObjectURL(event.target.files[0]);
         var imagediv = document.getElementById('preview');
@@ -15,7 +16,7 @@ function AddPhoto() {
     return (
         <div className="admin-area">
             <h2>Add Form</h2>
-            <form name="add-photo" data-netlify="true" onSubmit="submit">
+            <form name="add-photo" method="POST" data-netlify="true">
             <input type="hidden" name="form-name" value="add-photo" />
                 <div className="form-group">
                     <label>Name: <input type="text" name="name" className="form-control" placeholder="Enter Name" required /></label>
@@ -24,10 +25,10 @@ function AddPhoto() {
                     <label>Email: <input type="email" name="email" className="form-control" placeholder="Enter Email" required /></label>
                 </div>
                 <div className="form-group">
-                    <label>Phone No: <input type="text" name="phone-no" className="form-control" placeholder="Enter country code eg: +91 1234567890" required /></label>
+                    <label>Phone No: <input type="text" name="Phone No" className="form-control" placeholder="Enter country code eg: +91 1234567890" required /></label>
                 </div>
                 <div className="form-group">
-                    <label>Photo of Ad: <input type="file" id="file" name="photo" className="form-control" style={{ height: "50px" }} onChange={getImagePreview} required /></label>
+                    <label>Photo of Ad: <input type="file" name="photo" className="form-control" style={{ height: "50px" }} onChange={getImagePreview} required /></label>
                 </div>
                 <div id="preview">PREVIEW</div>
                 <div data-netlify-recaptcha="true" className="recaptcha" ></div>
