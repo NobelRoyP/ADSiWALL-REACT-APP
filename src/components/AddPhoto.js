dc725ecf934eac09d98be9cf90fcd841f8d76edf
NobelRoyP <101279727+NobelRoyP@users.noreply.github.com>
2022-05-05 23:28:45 +0530

    Add files via upload

--- src/components/AddPhoto.js
+++ src/components/AddPhoto.js
@@ -3,7 +3,7 @@ import '../AddPhoto.css'
 import 'bootstrap'
 
 function AddPhoto() {
-    /*function getImagePreview(event) {
+    function getImagePreview(event) {
         var image = URL.createObjectURL(event.target.files[0]);
         var imagediv = document.getElementById('preview');
         var newimg = document.createElement('img');
@@ -11,24 +11,27 @@ function AddPhoto() {
         newimg.src = image;
         newimg.width = "300";
         imagediv.appendChild(newimg);
-    }*/
+    }
     return (
         <div className="admin-area">
-
-            <form name="contact" method="post">
-                <input type="hidden" name="form-name" value="contact" />
-                <p>
-                    <label>Your Name: <input type="text" name="name" /></label>
-                </p>
-                <p>
-                    <label>Your Email: <input type="email" name="email" /></label>
-                </p>
-                <p>
-                    <label>Message: <textarea name="message"></textarea></label>
-                </p>
-                <p>
-                    <button type="submit">Send</button>
-                </p>
+            <h2>Add Form</h2>
+            <form name="add-photo" method="post" data-netlify="true" onSubmit="submit">
+            <input type="hidden" name="form-name" value="add-photo" />
+                <div className="form-group">
+                    <label>Name: <input type="text" name="name" className="form-control" placeholder="Enter Name" required /></label>
+                </div>
+                <div className="form-group">
+                    <label>Email: <input type="email" name="email" className="form-control" placeholder="Enter Email" required /></label>
+                </div>
+                <div className="form-group">
+                    <label>Phone No: <input type="text" name="phone-no" className="form-control" placeholder="Enter country code eg: +91 1234567890" required /></label>
+                </div>
+                <div className="form-group">
+                    <label>Photo of Ad: <input type="file" name="photo" className="form-control" style={{ height: "50px" }} onChange={getImagePreview} required /></label>
+                </div>
+                <div id="preview">PREVIEW</div>
+                <div data-netlify-recaptcha="true" className="recaptcha" ></div>
+                <button type="submit" className="btn btn-success" >send</button>
             </form>
         </div>
     )
