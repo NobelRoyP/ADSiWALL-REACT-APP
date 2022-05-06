dced76b0bd238f4dab95c9e76b3dd0b88fa799db
NobelRoyP <101279727+NobelRoyP@users.noreply.github.com>
2022-05-06 20:24:22 +0530

    Create Thanks.js

---
+++ src/components/Thanks.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../App.css'
+
+const Thanks = () => {
+    return (
+        <div className="About-area">
+            <h2>Thank You 🙏</h2>
+        </div>
+    )
+}
+export default Thanks
