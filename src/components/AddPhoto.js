db691e15c75ed9ec4e900ca029af90337eecd3df
NobelRoyP <101279727+NobelRoyP@users.noreply.github.com>
2022-05-07 16:40:35 +0530

    Update AddPhoto.js

--- src/components/AddPhoto.js
+++ src/components/AddPhoto.js
@@ -16,7 +16,25 @@ const AddPhoto = () => {
     return (
         <div className="admin-area">
             <h2>Add Form</h2>
-            
+            <form name="add-photo" method="POST" action="https://formsubmit.co/adsiwallweb@yahoo.com" enctype="multipart/form-data">
+            <input type="hidden" name="form-name" value="add-photo" />
+                <div className="form-group">
+                    <label>Name: <input type="text" name="name" className="form-control" placeholder="Enter Name" required /></label>
+                </div>
+                <div className="form-group">
+                    <label>Email: <input type="email" name="email" className="form-control" placeholder="Enter Email" required /></label>
+                </div>
+                <div className="form-group">
+                    <label>Phone No: <input type="text" name="phone" className="form-control" placeholder="enter code first:- +91" required /></label>
+                </div>
+                <div className="form-group">
+                    <label>Photo of Ad: <input type="file" name="attachment" className="form-control" style={{ height: "50px" }} onChange={getImagePreview} required accept="image/png, image/jpeg"/></label>
+                </div>
+                <div id="preview">PREVIEW</div>
+                <button type="submit" className="btn btn-success" >send</button>
+                <input type="hidden" name="_next" value="https://adsiwall.netlify.app/thanks" />
+                <input type="hidden" name="_captcha" value="false"/>
+            </form>
         </div>
     )
 }
