2ffff9c3feeecb4d56bffbf5b162e3de96fb2cd9
NobelRoyP <101279727+NobelRoyP@users.noreply.github.com>
2022-05-22 23:53:11 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,26 +3,15 @@ import Images from './images'
 
 export class Home extends Component {
      render() {
-        const showDiv = () => {
-        var ImageLink = document.getElementById("loadingDiv");
-     
-        ImageLink.style.opacity="1";
-        }
-        setTimeout(() => {
-        var ImageLink = document.getElementById("loadingDiv");
-     
-        ImageLink.style.opacity="0";
-        }, 2000);
         return (
             <div>
                 <div className="poster-area">
                 <div className="poster">
                     {
                         Images.map((Image)=>(
-                            <a href={Image.link} onClick={showDiv}><img src={Image.src} alt={Image.title} key={Image.id}/></a>
+                            <a href={Image.link}><img src={Image.src} alt={Image.title} key={Image.id}/></a>
                         ))
                     }
-                    <div id="loadingDiv" className="loader">Loading...</div>
                 </div>
             </div>
             </div>
