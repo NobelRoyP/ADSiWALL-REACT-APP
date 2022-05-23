ae1c23b350b2b2b42328a2ad7df033767826090d
NobelRoyP <101279727+NobelRoyP@users.noreply.github.com>
2022-05-23 23:12:24 +0530

    Create Videos.js

---
+++ src/components/Videos.js
@@ -0,0 +1,11 @@
+import { v4 as uuidv4 } from 'uuid'
+const Videos = [
+    {
+        src: "",
+        title: "Poster",
+        id: uuidv4(),
+        link: "https://nobsownshop.netlify.app", 
+    },
+    
+]
+export default Videos
