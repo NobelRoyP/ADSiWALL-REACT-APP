02b88c419e6ae73d5208e2db717885b243b2b585
NobelRoyP <101279727+NobelRoyP@users.noreply.github.com>
2022-05-26 15:17:29 +0530

    Update images.js

--- src/components/images.js
+++ src/components/images.js
@@ -7,5 +7,12 @@ const Images = [
         id: uuidv4(),
         link: "https://nobsownshop.netlify.app", 
     },
+    {
+        vidsrc: "https://d3cvwyf9ksu0h5.cloudfront.net/answer-1562685941_17241555.mp4",
+        imgsrc: "./Images/favicon.png",
+        title: "Poster",
+        id: uuidv4(),
+        link: "https://nobsownshop.netlify.app", 
+    },
 ]
 export default Images
