e9c48295b44ebeefcd714c462e210bb4ab0bbaee
NobelRoyP <101279727+NobelRoyP@users.noreply.github.com>
2022-05-28 17:39:36 +0530

    Update images.js

--- src/components/images.js
+++ src/components/images.js
@@ -1,14 +1,12 @@
 import { v4 as uuidv4 } from 'uuid'
 const Images = [
     {
-        vidsrc: "https://d3cvwyf9ksu0h5.cloudfront.net/answer-1562685941_17241555.mp4",
-        imgsrc: "./Images/favicon.png",
+        imgsrc: "./Images/iamnoelwebpic.jpeg",
         title: "Poster",
         id: uuidv4(),
         link: "https://nobsownshop.netlify.app", 
     },
     {
-        vidsrc: "https://d3cvwyf9ksu0h5.cloudfront.net/answer-1562685941_17241555.mp4",
         imgsrc: "./Images/favicon.png",
         title: "Poster",
         id: uuidv4(),
