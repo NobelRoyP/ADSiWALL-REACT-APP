87f819b0e8d1e7d1b3d0ce80aa9694fd15c902e2
NobelRoyP <101279727+NobelRoyP@users.noreply.github.com>
2022-05-28 18:18:11 +0530

    Update images.js

--- src/components/images.js
+++ src/components/images.js
@@ -1,12 +1,14 @@
 import { v4 as uuidv4 } from 'uuid'
 const Images = [
     {
+        name: "i am Noel", 
         imgsrc: "./Images/iamnoelwebpic.jpeg",
         title: "Poster",
         id: uuidv4(),
         link: "https://www.instagram.com/p/CeBeb36BLV7/", 
     },
     {
+        name: "Adsiwall", 
         imgsrc: "./Images/favicon.png",
         title: "Poster",
         id: uuidv4(),
