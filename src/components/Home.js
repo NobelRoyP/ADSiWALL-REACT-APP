b82de6ad18e0347addb404eda96762415dfdbac3
NobelRoyP <101279727+NobelRoyP@users.noreply.github.com>
2022-05-28 18:43:23 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ export class Home extends Component {
                         Images.map((Image)=>(
                          <div className="ad-post">
                             <a href={Image.link}><img src={Image.imgsrc} alt={Image.title} key={Image.id}/></a>
-                            <a href={Image.link}><div className="link">Open</div></a>
+                            <a href={Image.link}><div className="link">Open<i class="fa-solid fa-arrow-up-right-from-square"></i></div></a>
                          </div>
                         ))
                     }
