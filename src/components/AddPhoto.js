4eb38813579bbf198de8cb9e4ed9c72420fd4848
NobelRoyP <101279727+NobelRoyP@users.noreply.github.com>
2022-06-03 17:37:10 +0530

    Update AddPhoto.js

--- src/components/AddPhoto.js
+++ src/components/AddPhoto.js
@@ -28,20 +28,20 @@ const AddPhoto = () => {
             <form name="add-photo" method="POST" action="https://formsubmit.co/adsiwallweb@yahoo.com" enctype="multipart/form-data">
             <input type="hidden" name="form-name" value="add-photo" />
                 <div className="form-group">
-                    <label>Name: <input type="text" name="name" className="form-control" placeholder="Enter Name" required /></label>
+                    <label>Name: <input type="text" name="name" className="form-control" placeholder="Enter Name" required autocomplete="off"/></label>
                 </div>
                 <div className="form-group">
                     <label>Email: <input type="email" name="email" className="form-control" placeholder="Enter Email" required autocomplete="off"/></label>
                 </div>
                 <div className="form-group">
-                    <label>Phone No: <input type="text" name="phone" className="form-control" placeholder="enter code first:- +91" required /></label>
+                    <label>Phone No: <input type="text" name="phone" className="form-control" placeholder="enter code first:- +91" required autocomplete="off"/></label>
                 </div>
                 <div className="form-group">
                     <label>Photo of Ad: <input type="file" name="attachment" className="form-control" style={{ height: "50px" }} onChange={getImagePreview} required accept="image/png, image/jpeg"/></label>
                 </div>
                 <div id="preview">PREVIEW</div>
                 <div className="form-group">
-                    <label>URL: <input type="text" name="link" className="form-control" placeholder="link to your website,Instagram,Facebook,etc..." required /></label>
+                    <label>URL: <input type="text" name="link" className="form-control" placeholder="link to your website,Instagram,Facebook,etc..." required autocomplete="off"/></label>
                 </div>
                 <button type="submit" className="btn btn-success" id="btn" onClick={validationBtn}>Send</button>
                 <input type="hidden" name="_next" value="https://adsiwall.netlify.app/thanks" />
