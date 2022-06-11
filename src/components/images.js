880a3949ca3a4b1c2fe3fa0143264b450c359ad8
NobelRoyP <101279727+NobelRoyP@users.noreply.github.com>
2022-06-11 09:34:30 +0530

    Update images.js

--- src/components/images.js
+++ src/components/images.js
@@ -1,5 +1,19 @@
 import { v4 as uuidv4 } from 'uuid'
 const Images = [
+     /*{
+        brand_name: "", 
+        imgsrc: "./Images/",
+        title: "Poster",
+        id: uuidv4(),
+        link: "", 
+    },*/
+    /*{
+        brand_name: "", 
+        imgsrc: "./Images/",
+        title: "Poster",
+        id: uuidv4(),
+        link: "", 
+    },*/
     /*{
         brand_name: "", 
         imgsrc: "./Images/",
