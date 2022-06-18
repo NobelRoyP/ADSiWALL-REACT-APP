165dfed3514579ecc29a2418427fbae945186da8
NobelRoyP <101279727+NobelRoyP@users.noreply.github.com>
2022-06-18 17:54:41 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from "./components/Header";
 import About from "./components/About";
-import Home from "./components/Home";
+import Home from "./components/Categories/Home";
 import Contact from './components/Contact';
 import AddPhoto from './components/AddPhoto';
-import Thanks from './components/Thanks';
+import Youtube from './components/Categories/Youtube'
+import Website from './components/Categories/Website'
+import ShoppingSite from './components/Categories/ShoppingSite'
+import Facebook from './components/Categories/Facebook'
+import Twitter from './components/Categories/Twitter'
+import Blog from './components/Categories/Blog'
+
 import './App.css';
 
 function App() {
@@ -13,13 +19,18 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/contact" element={<Contact/>} />
-          <Route path="/add-photo" element={<AddPhoto/>} />
-          <Route path="/thanks" element={<Thanks/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/add-photo" element={<AddPhoto />} />
+          <Route path="/youtube" element={<Youtube />} />
+          <Route path="/website" element={<Website />} />
+          <Route path="/shoppingsite" element={<ShoppingSite />} />
+          <Route path="/facebook" element={<Facebook />} />
+          <Route path="/twitter" element={<Twitter />} />
+          <Route path="/blog" element={<Blog />} />
         </Routes>
-        <Header/>
+        <Header />
       </Router>
     </div>
   );
