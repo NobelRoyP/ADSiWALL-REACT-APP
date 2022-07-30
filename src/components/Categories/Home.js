5b2e948469fa62cb8e44ca24ea6912d5895d0b71
NobelRoyP <101279727+NobelRoyP@users.noreply.github.com>
2022-07-30 19:17:59 +0530

    Update Home.js

--- src/components/Categories/Home.js
+++ src/components/Categories/Home.js
@@ -18,17 +18,6 @@ export class Home extends Component {
                     </div>
 
                 </div>
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6673846975177036" crossorigin="anonymous"></script>
-<!-- Test -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-6673846975177036"
-     data-ad-slot="9437493746"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
             </div>
         )
     }
